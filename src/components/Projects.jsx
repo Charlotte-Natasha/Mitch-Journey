a9755fee9740fac8a10aafe47d32869c162c620f
Charlotte Natasha <natashaonyango97@gmail.com>
2022-11-14 14:00:54 +0100

    styled the images in grid

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -8,48 +8,56 @@ import Telkom from '../images/porfolio/telkom.png'
 
 const Projects = () => {
     return (
-        <section className='min-h-screen text-white bg-black pt-20 pb-28 px-8'>
+        <div className='min-h-screen text-white bg-black pt-20 pb-28 px-8'>
             <div className='max-w-6xl mx-auto'>
                 <div>
                     <h4 className='text-gray-500'> My Projects</h4>
                     <h1 className='pt-2 text-4xl max-w-md'>Below are some of my projects</h1>
                 </div>
-                <div className='grid grid-cols-1 bg-[#181823] mt-16'>
-                    <div className='rounded-md shadow-md '>
-                        <img src={Orac} alt="porfolio" className='w-full h-82 object-cover'/>
-                        <div className='p-8'>
+
+                <div className='mt-16 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5'>
+
+                    <div className='overflow-hidden rounded-md shadow-lg bg-[#181823]'>
+                        <img src={Orac} alt="porfolio" className='w-full'/>
+                        
+                        <div className='p-4'>
                             <h1 className='text-3xl'>Orac Website Design</h1>
                             <p className='text-gray-500 py-3'>I worked with the Orac team, a startup, to design their entire website and mobile view from both Android and IOS. This project lasted a month and it was one of my best works.</p>
-                            <a href="https://www.behance.net/gallery/150736879/Orac-Website" className='text-right'><img src={Circle} alt="" target="blank"/></a>
+                            <a href="https://www.behance.net/gallery/150736879/Orac-Website" className='float-right'><img src={Circle} alt="" target="blank"/></a>
                         </div>
-                        <div className='rounded-md shadow-md '>
-                        <img src={Hisani} alt="porfolio" className='w-full h-82 object-cover'/>
-                        <div className='p-8'>
-                            <h1 className='text-3xl'>Orac Website Design</h1>
+                        
+                    </div>
+
+                    <div className='rounded overflow-hidden shadow-lg bg-[#181823]'>
+                        <img src={Hisani} alt="porfolio" className='w-full'/>
+                        <div className='p-4'>
+                            <h1 className='text-3xl'>Hisani - Donation app and website</h1>
                             <p className='text-gray-500 py-3'>I worked with the Orac team, a startup, to design their entire website and mobile view from both Android and IOS. This project lasted a month and it was one of my best works.</p>
-                            <a href="https://www.behance.net/gallery/150736879/Orac-Website" className='text-right'><img src={Circle} alt="" target="blank"/></a>
+                            <a href="https://www.behance.net/gallery/150736879/Orac-Website" className='float-right'><img src={Circle} alt="" target="blank"/></a>
                         </div>
                     </div>
-                    <div className='rounded-md shadow-md '>
-                        <img src={Switchlife} alt="porfolio" className='w-full h-82 object-cover'/>
-                        <div className='p-8'>
-                            <h1 className='text-3xl'>Orac Website Design</h1>
-                            <p className='text-gray-500 py-3'>I worked with the Orac team, a startup, to design their entire website and mobile view from both Android and IOS. This project lasted a month and it was one of my best works.</p>
-                            <a href="https://www.behance.net/gallery/150736879/Orac-Website" className='text-right'><img src={Circle} alt="" target="blank"/></a>
+
+                    <div className='rounded overflow-hidden shadow-lg bg-[#181823]'>
+                        <img src={Switchlife} alt="porfolio" className='w-full'/>
+                        <div className='p-4'>
+                            <h1 className='text-3xl'>Switchlife mental health app</h1>
+                            <p className='text-gray-500 py-3'>This was a collaborative with 4 other designers where we brought an idea to life, a pro bono mental health app. This was my second case study as I ventured into UX design.</p>
+                            <a href="https://www.behance.net/gallery/150736879/Orac-Website" className='float-right'><img src={Circle} alt="" target="blank"/></a>
                         </div>
                     </div>
-                    <div className='rounded-md shadow-md '>
-                        <img src={Telkom} alt="porfolio" className='w-full h-82 object-cover'/>
-                        <div className='p-8'>
-                            <h1 className='text-3xl'>Orac Website Design</h1>
-                            <p className='text-gray-500 py-3'>I worked with the Orac team, a startup, to design their entire website and mobile view from both Android and IOS. This project lasted a month and it was one of my best works.</p>
-                            <a href="https://www.behance.net/gallery/150736879/Orac-Website" className='text-right'><img src={Circle} alt="" target="blank"/></a>
+
+                    <div className='rounded overflow-hidden shadow-lg bg-[#181823]'>
+                        <img src={Telkom} alt="porfolio" className='w-full'/>
+                        <div className='p-4'>
+                            <h1 className='text-3xl'>Telkom App Redesign</h1>
+                            <p className='text-gray-500 py-3'>This was my first UX case study. I designed and prototyped a Telkom mobile app specifically for IOS. This app is meant to give Telkom users a great and easy experience when interacting with their products.</p>
+                            <a href="https://www.behance.net/gallery/150736879/Orac-Website" className='float-right'><img src={Circle} alt="" target="blank"/></a>
                         </div>
                     </div>
-                    </div>
+
                 </div>
             </div>
-        </section>
+        </div>    
     )
 }
 
