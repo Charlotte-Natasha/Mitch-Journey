d2de835a5046871e0c7675732c4458dd8076db38
Charlotte Natasha <natashaonyango97@gmail.com>
2022-11-14 11:07:34 +0100

    styled footer and added links

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,12 +3,17 @@ import Book from '../images/about.jpeg'
 
 const About = () => {
     return (
-        <div className='bg-[#181823] h-screen flex flex-col md:flex-row md:items-center md:justify-between'>
+        <div className='bg-[#181823] h-screen flex flex-col md:flex-row md:items-center md:justify-between text-white'>
             <div className='p-8 pt-1'>
-                <h1 className='text-white text-4xl '>About Me</h1>
-                <p className='text-gray-500 max-w-md mt-4 md:flex-1'>Customers need great experiences with every interaction- that's where I come in. <br /> As a User Experience Designer, it's my job to provide exceptional services. I've been doing it for 2 years and have loved every minute of it. <br /> Problem Solving and coming up with profitable solutions is truly a passion for me. Naturally, this developed into a career which has led me to where I am today.</p>
+                <h1 className='text-4xl '>About Me</h1>
+                <p className='text-gray-500 max-w-md mt-4 md:flex-1 '>Customers need great experiences with every interaction- that's where I come in. <br /> As a User Experience Designer, it's my job to provide exceptional services. I've been doing it for 2 years and have loved every minute of it. <br /> Problem Solving and coming up with profitable solutions is truly a passion for me. Naturally, this developed into a career which has led me to where I am today.</p>
+
+                <div className='py-5'>
+                <p>More about me</p>
+                </div>
             </div>
-            <div className='md:flex-1 md:w-auto h-1/2'>
+            
+            <div className='md:flex-1 md:w-auto h-1/2 object-cover'>
             <img src={Book} alt="my profile" className='h-[100%]'/>
             </div>
         </div>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,19 +1,35 @@
 import React from 'react'
+import Tweet from '../images/twitter.png'
+import Behance from '../images/behance.png'
+import Linked from '../images/linked.png'
 
 const Footer = () => {
     return (
-        <div className='h-40 w-full bg-[#181823] text-white p-4'>
+        <div className='h-50 w-full bg-[#181823] text-white p-4'>
             <div className='items-center justify-center md:flex md:justify-between'>
                 <div className='flex flex-col justify-between space-y-8'>
-                    <div>
+                    <div className='text-4xl'>
                         <h1>Michelle.O</h1>
                     </div>
-                    <div>
-                        
+                    <div className='flex space-x-4 text-lg'>
+                        <a href="https://twitter.com/Michelle_Okello" target='blank'>
+                            <img src={Tweet} alt="icon" className='w-10 h-10' />
+                        </a>
+                        <a href="https://www.behance.net/Michelle_Okello?tracking_source=search_users|michelle_okello" target='blank'>
+                            <img src={Behance} alt="icon" className='w-10 h-10' />
+                        </a>
+                        <a href="https://www.linkedin.com/in/michelle-okello-954920145/" target='blank'>
+                            <img src={Linked} alt="icon" className='w-10 h-10' />
+                        </a>
                     </div>
+                    
                 </div>
+                
             </div>
-            Footer
+            <div className='py-5'>
+                <p> Copyright 2022 . <a href="https://github.com/Charlotte-Natasha/Mitch-Journey" target='blank'> Charlotte Natasha</a></p>
+            </div>
+            
         </div>
     )
 }
