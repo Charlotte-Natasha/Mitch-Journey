2b4eaeb791107046baa9b86eb2761fe186a3ea6a
Charlotte Natasha <natashaonyango97@gmail.com>
2022-11-16 14:28:39 +0100

    installed react scroll

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,7 @@ const About = () => {
     return (
         <section name='about' className='h-screen flex flex-col md:flex-row md:items-center md:justify-between text-white bg-[#181823]'>
             
-            <div className='grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 '>
+            <div className=' '>
                 <div className='p-8 pt-1'>
                     <h1 className='text-4xl '>About Me</h1>
                     <p className='text-gray-500 max-w-md mt-4 md:flex-1 '>Customers need great experiences with every interaction- that's where I come in. <br /> As a User Experience Designer, it's my job to provide exceptional services. I've been doing it for 2 years and have loved every minute of it. <br /> Problem Solving and coming up with profitable solutions is truly a passion for me. Naturally, this developed into a career which has led me to where I am today.</p>
@@ -15,7 +15,7 @@ const About = () => {
                     </div>
                 </div>
             </div>
-            <section className='bg-book bg-cover bg-center h-screen w-full'>
+            <section className='bg-book bg-cover bg-center h-screen w-1/2'>
 
             </section>
         </section>
